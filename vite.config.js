221649b405c7844f511b0b46c2379f4de6db7355
Nyi Nyi <na5112063@gmail.com>
2026-01-08 20:44:50 +0630

    Fix build error

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-    tailwindcss(),
+    react(),      // official React plugin, no babel plugins needed
+    tailwindcss() // Tailwind integration
   ],
-})
+  build: {
+    target: 'esnext', // important for React 18
+  },
+});
